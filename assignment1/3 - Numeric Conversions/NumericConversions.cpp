882f6636ce3d4980a1fbc05bf8b7d72bb3d0c98a
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-01-08 17:13:38 +0300

    done problems 0-2

---
+++ assignment1/3 - Numeric Conversions/NumericConversions.cpp
@@ -0,0 +1,43 @@
+/*
+ * File: NumericConversions.cpp
+ * ---------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Numeric Conversions problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "strlib.h"
+using namespace std;
+
+/* Function prototypes */
+
+string intToString(int n);
+int stringToInt(string str);
+
+/* Main program */
+
+int main() {
+
+    return 0;
+}
+
+string intToString(int n) {
+    return "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
