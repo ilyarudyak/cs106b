18fdda39e9cc947cf238dbf602b170fca7957380
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-01-08 21:32:31 +0300

    done problems 3

--- assignment1/3 - Numeric Conversions/NumericConversions.cpp
+++ assignment1/3 - Numeric Conversions/NumericConversions.cpp
@@ -9,8 +9,9 @@
 
 #include <iostream>
 #include <string>
-#include "console.h"
+//#include "console.h"
 #include "strlib.h"
+#include <cmath>
 using namespace std;
 
 /* Function prototypes */
@@ -21,13 +22,55 @@ int stringToInt(string str);
 /* Main program */
 
 int main() {
-
+    cout << stringToInt("42") << endl;
     return 0;
 }
 
-string intToString(int n) {
-    return "";
+int stringToInt2(string s, int n, int k) {
+    
+    if (s.length() == 1) {
+        return (s[0] - (int ) '0') * pow(10, k) + n;
+    }
+    return stringToInt2(s.substr(0, s.length() - 1),
+                        (n + s[s.length() - 1] - (int) '0') * pow(10, k),
+                        k + 1);
 }
+
+int stringToInt(string str) {
+    return stringToInt2(str, 0, 0);
+}
+
+string intToString2(string s, int n) {
+    
+    
+    if (n < 10) {
+        string s1 = (string(1, char(n + '0'))) + s;
+        return s1;
+    }
+    
+    string s2 = (string(1, char(n % 10 + '0'))) + s;
+    return intToString2(s2, n / 10);
+}
+
+string intToString(int n) {
+    return intToString2("", n);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
