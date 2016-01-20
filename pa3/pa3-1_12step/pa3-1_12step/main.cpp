06fb839079b6adc1e6a68357702ba1d0d564b37f
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-01-20 18:53:33 +0300

    done warm-up (1st draft)

---
+++ pa3/pa3-1_12step/pa3-1_12step/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pa3-1_12step
+//
+//  Created by Ilya Rudyak on 1/20/16.
+//  Copyright © 2016 Ilya Rudyak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
