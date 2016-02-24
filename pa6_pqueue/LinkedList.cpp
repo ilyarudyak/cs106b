4d1ee79f219aa73b3315d9a3ee84065ebe025138
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-02-24 21:58:10 +0300

    initial commit

---
+++ pa6_pqueue/LinkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ilya Rudyak on 2/24/16.
+//
+
+#include "LinkedList.h"
---
+++ pa6_pqueue/LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by Ilya Rudyak on 2/24/16.
+//
+
+#ifndef PA6_PQUEUE_LINKEDLIST_H
+#define PA6_PQUEUE_LINKEDLIST_H
+
+
+class LinkedList {
+
+};
+
+
+#endif //PA6_PQUEUE_LINKEDLIST_H
