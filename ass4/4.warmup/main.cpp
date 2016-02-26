ebcc64b0fe7eb318c11413c7cacae1a6250b2ab5
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-02-26 21:32:07 +0300

    done warm-up 1st part

--- ass4/4.warmup/main.cpp
+++ ass4/4.warmup/main.cpp
@@ -1,8 +1,93 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Node {
+    int data;
+    Node *next;
+};
+
+
+void addToHead(Node *&head, int data) {
+    Node *newNode = new Node;
+    newNode->data = data;
+    // this works even if head == nullptr
+    newNode->next = head;
+    head = newNode;
+
+};
+void addToTail(Node *&tail, int data) {
+    Node *newNode = new Node;
+    newNode->data = data;
+    // this works even if head == nullptr
+    newNode->next = nullptr;
+
+    tail->next = newNode;
+    tail = newNode;
+
+};
+void printList(Node *head) {
+    for (Node *walker = head; walker != nullptr; walker = walker->next) {
+        if (walker->next != nullptr) {
+            cout << walker->data << "->";
+        } else {
+            cout << walker->data;
+        }
+    }
+    cout << endl;
+}
+Node* convertToList(vector<int> v) {
+
+    Node *head = nullptr;
+
+    // add 0 node
+    addToHead(head, v[0]);
+    Node *tail = head;
+
+    // add other nodes
+    for (int i = 1; i < v.size(); ++i) {
+        addToTail(tail, v[i]);
+    }
+    return head;
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    vector<int> v = {1, 2, 3, 4, 5};
+    printList(convertToList(v));
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
