e760d5e2d53800cb2f553a491453623d095bae58
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-03 16:18:10 +0300

    add some basic functions

--- parlante/linked_list/main.cpp
+++ parlante/linked_list/main.cpp
@@ -2,7 +2,80 @@
 
 using namespace std;
 
+struct Node {
+    int data;
+    Node* next;
+};
+Node *newNode(int data) {
+    Node *node = new Node;
+    node->data = data;
+    node->next = nullptr;
+    return node;
+}
+Node *buildOneTwoThree() {
+    Node *node = newNode(1);
+    node->next = newNode(2);
+    node->next->next = newNode(3);
+    return node;
+}
+void printList(Node *node) {
+    if (node == nullptr) {
+        cout << "the list is empty ..." << endl;
+        return;
+    }
+
+    if (node->next == nullptr) {
+        cout << node->data << endl;
+    } else {
+        cout << node->data << "->";
+        printList(node->next);
+    }
+}
+int length(Node *node) {
+    if (node == nullptr) {
+        return 0;
+    }
+
+    return 1 + length(node->next);
+}
+void pushWith2Ptr(Node **head, int data) {
+    Node *node = newNode(data);
+    node->next = *head;
+    *head = node;
+}
+void push(Node *&head, int data) {
+    Node *node = newNode(data);
+    node->next = head;
+    head = node;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    // test basic functions
+//    Node *head = buildOneTwoThree();
+//    printList(head);
+//    cout << length(head) << endl;
+
+    // test push()
+    Node *head = nullptr;
+    push(head, 3); push(head, 2); push(head, 1);
+    printList(head);
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
